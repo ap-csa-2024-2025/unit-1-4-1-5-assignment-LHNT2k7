c25cdbe87690cd61b1c4527dca8f33864bb7ea64
LHNT2k7 <ltri9573@gmail.com>
2024-09-11 09:52:19 -0700

    haha

--- Problem0.java
+++ Problem0.java
@@ -3,8 +3,8 @@ public class Problem0
   public static void main(String[] args)
   {
     double value = 7;
-    int value2 = 3.0;
-    System.out.println(value2);+
     System.out.println(value);
+    int value2 = 3.0;
+    System.out.println(value2);
   }
 }
