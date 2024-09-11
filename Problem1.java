c906f0daecc1e5b45da6b2c5ebd9e9fd9f3910d9
LHNT2k7 <ltri9573@gmail.com>
2024-09-11 10:04:20 -0700

    lun

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,24 @@
-
+public class Problem1
+{
+  public static void main(String[] args)
+  {
+    int So1 = 24;
+    System.out.println("The number is " + So1);
+    So1++;
+    System.out.println("The number now " + So1);
+    So1++;
+    System.out.println("The number now " + So1);
+    So1++;
+    System.out.println("The number now " + So1);
+    So1++;
+    System.out.println("The number now " + So1);
+    So1--;
+    System.out.println("The number now " + So1);
+    So1--;
+    System.out.println("The number now " + So1);
+    So1--;
+    System.out.println("The number now " + So1);
+    So1--;
+    System.out.println("The number now " + So1);
+  }
+}
