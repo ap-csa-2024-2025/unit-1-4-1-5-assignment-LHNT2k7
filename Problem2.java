f91fc7b03d66f2987d244d1e4ea0d4c4d3d265e7
LHNT2k7 <ltri9573@gmail.com>
2024-09-11 10:12:42 -0700

    huhu

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,11 @@
-
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    int So3 = 257;
+    System.out.println("The number is " + So3);
+    System.out.println(So3 % 10);
+    int So3 = So3 - (So3 % 10);
+    System.out.println(So3 % 100);
+    int So3 = So3 - (So3 % 100);
+    System.out.println(So3 % 1000);
